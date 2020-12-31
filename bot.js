5001cb1660d404272d18d5bdee514d64fce40f09
M31 Andromeda <abhinavsriram1331@gmail.com>
2020-12-31 13:06:11 +0000

    Changed Formatting

--- bot.js
+++ bot.js
@@ -6,38 +6,38 @@ const client = new Discord.Client();
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
-    console.log("Logged In");
+  console.log("Logged In");
 }
 
 const replies = [
-    'Pong',
-    'Yes',
-    'Pong!'
+  'Pong',
+  'Yes',
+  'Pong!'
 ]
 client.on('message', gotMessage);
 
 async function gotMessage(msg) {
-    console.log(msg.content);
+  console.log(msg.content);
 
-    let tokens = msg .content.split(' ');
+  let tokens = msg.content.split(' ');
 
-    if (tokens[0] === '^ping') {
-        const pongs = Math.floor(Math.random() * replies.length);
-        msg.channel.send(replies[pongs])
-    }else if (tokens[0] == '^gif') {
+  if (tokens[0] === '^ping') {
+    const pongs = Math.floor(Math.random() * replies.length);
+    msg.channel.send(replies[pongs])
+  } else if (tokens[0] == '^gif') {
 
-        let keywords = 'space'
-        if (tokens.length > 1) {
-            keywords = tokens.slice(1, tokens.length).join(" ");
-        }
-
-        let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=high`
-        let response = await fetch(url);
-        let json = await response.json();
-        console.log(json);
-        let index = Math.floor(Math.random() * json.results.length);
-        msg.channel.send(json.results[index].url);
+    let keywords = 'space'
+    if (tokens.length > 1) {
+      keywords = tokens.slice(1, tokens.length).join(" ");
     }
+
+    let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=high`
+    let response = await fetch(url);
+    let json = await response.json();
+    console.log(json);
+    let index = Math.floor(Math.random() * json.results.length);
+    msg.channel.send(json.results[index].url);
+  }
 }
 
 client.login(process.env.BOT_TOKEN);
