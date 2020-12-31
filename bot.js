b175c859400a10a1b7a674541cf2874abd1bc955
M31 Andromeda <abhinavsriram1331@gmail.com>
2020-12-31 12:57:14 +0000

    Initial commit

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+require('dotenv').config();
+const client = new Discord.Client();
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log("Logged In");
+}
+
+const replies = [
+    'Pong',
+    'Yes',
+    'Pong!'
+]
+client.on('message', gotMessage);
+
+async function gotMessage(msg) {
+    console.log(msg.content);
+
+    let tokens = msg .content.split(' ');
+
+    if (tokens[0] === '^ping') {
+        const pongs = Math.floor(Math.random() * replies.length);
+        msg.channel.send(replies[pongs])
+    }else if (tokens[0] == '^gif') {
+
+        let keywords = 'space'
+        if (tokens.length > 1) {
+            keywords = tokens.slice(1, tokens.length).join(" ");
+        }
+
+        let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=high`
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        let index = Math.floor(Math.random() * json.results.length);
+        msg.channel.send(json.results[index].url);
+    }
+}
+
+client.login(process.env.BOT_TOKEN);
